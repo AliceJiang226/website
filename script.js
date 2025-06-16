1bfbd051e4a3caaf618f6c3d1387a3526699b353
AliceJiang226 <alice.jiang@kinkaid.org>
2025-06-16 12:10:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('helloBtn').addEventListener('click', function() {
+    alert('Hello from your new website!');
+});
+
+function flipCard(card) {
+    card.classList.toggle('flipped');
+}
